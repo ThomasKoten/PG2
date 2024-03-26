9fc0560c8a843732ba6be8bb8aba4e94d31b1216
KotenTomas <tomas.koten@tul.cz>
2024-03-26 22:07:32 +0100

    Nářadí na horší časy

---
+++ StringUtils.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <algorithm> 
+#include <cctype>
+#include <locale>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+//@Bahamut731lp
+// trim from start (in place)
+inline void ltrim(std::string& s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+        }));
+}
+
+// trim from end (in place)
+inline void rtrim(std::string& s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+        }).base(), s.end());
+}
+
+std::vector<std::string> split(const std::string& str, char delimiter) {
+    std::istringstream iss(str);
+    std::vector<std::string> tokens;
+    std::string token;
+    while (std::getline(iss, token, delimiter)) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
